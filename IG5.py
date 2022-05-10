5f63170be8d64af1d7a27bb4e27b0cc59c89d497
Jorge Gomez <57576609+JorgeGomezAZ@users.noreply.github.com>
2022-05-10 15:36:21 -0500

    abecedario con 2 modelos

--- IG5.py
+++ IG5.py
@@ -21,11 +21,12 @@ leyendo = False
 global valido
 valido = 0
 
-redConvs = [tf.keras.models.load_model('./modelos/modeloNumerosEstaticos.h5'),tf.keras.models.load_model('./modelos/modeloAbecedario.h5'),tf.keras.models.load_model('./modelos/modeloCuerpo.h5')]
+redConvs = [tf.keras.models.load_model('./modelos/modeloNumerosEstaticos.h5'),tf.keras.models.load_model('./modelos/modeloAbecedario_1.h5'),tf.keras.models.load_model('./modelos/modeloAbecedario_2s.h5'),tf.keras.models.load_model('./modelos/modeloCuerpo.h5')]
 modelos = {
 		0: ["1", "2", "3", "4", "5", "6", "7", "8"],
-		1: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "Y"],
-		2: ["Cuello", "Diente", "Espalda", "Estómago", "Hombro", "Lengua", "Mano", "Muñeca", "Nariz", "Pulgar", "Pelo"]
+		1: ["A", "B", "C", "D", "E", "G", "I", "M", "R", "S"],
+		2: ["F", "H", "L", "N", "O", "P", "T", "U", "V", "W", "Y"],
+		3: ["Cuello", "Diente", "Espalda", "Estómago", "Hombro", "Lengua", "Mano", "Muñeca", "Nariz", "Pulgar", "Pelo"]
 	}
 class Ui_IG5_Sena(object):
 	def setupUi(self, IG5_Sena):
